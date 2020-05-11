b50bc10438f5e973db98a3c917909d04501ccb96
Softler <tasc.softler@gmail.com>
2020-05-11 19:39:56 +0400

    finaly

--- src/views/Reports.vue
+++ src/views/Reports.vue
@@ -1,58 +1,54 @@
 <template>
-  <div class="home">
-    <v-container>
-        <v-row class="text-center">
-            <template>
+    <div class="home">
+        <v-container>
+            <v-row class="text-center">
                 <v-card>
-                    <v-data-table
-                            :headers="headers"
-                            :items="items"
-                            :page.sync="page"
-                            :items-per-page="itemsPerPage"
-                            hide-default-footer
-                            sort-by="calories"
-                            class="elevation-1"
-                    >
+                    <v-data-table :headers="headers" :items="items" :page.sync="page" :items-per-page="itemsPerPage"
+                        hide-default-footer sort-by="calories" class="elevation-1">
                         <template v-slot:top>
                             <v-toolbar flat color="white">
                                 <v-toolbar-title>Reports</v-toolbar-title>
                             </v-toolbar>
                         </template>
                     </v-data-table>
-                    <div class="text-center pt-2" >
+                    <div class="text-center pt-2">
                         <v-pagination v-model="page" :length="pageCount"></v-pagination>
                     </div>
                     <div>
-                         <v-btn color="primary" dark class="mb-2" @click="$router.push({ path:'/report/types'})">
-                         Create Report
-                         </v-btn>
+                        <v-btn color="primary" dark class="mb-2" @click="$router.push({ path:'/report/types'})">
+                            Create Report
+                        </v-btn>
                     </div>
                 </v-card>
-            </template>
-        </v-row>
-    </v-container>
-  </div>
+            </v-row>
+        </v-container>
+    </div>
 </template>
 
 <script>
     import axios from 'axios';
     const url = 'https://39c81456-e28f-4ca1-9487-d60ca5d9ef47.mock.pstmn.io/api/reports';
 
-export default {
-  name: 'Reports',
+    export default {
+        name: 'Reports',
         data: () => ({
-            items:[],
+            items: [],
             page: 1,
             itemsPerPage: 5,
-            headers: [
-                {
+            headers: [{
                     text: 'type',
                     align: 'start',
                     sortable: false,
                     value: 'type',
                 },
-                {text: 'name', value: 'name'},
-                {text: 'status', value: 'status'},
+                {
+                    text: 'name',
+                    value: 'name'
+                },
+                {
+                    text: 'status',
+                    value: 'status'
+                },
 
             ],
 
@@ -63,23 +59,29 @@ export default {
             },
         },
         watch: {
-            page:function(){
-                this.$router.replace({ path:'', query: {page:this.page} })
+            page: function () {
+                this.$router.replace({
+                    path: '',
+                    query: {
+                        page: this.page
+                    }
+                })
             },
         },
         methods: {
-            getData(){
-                axios.get(url).then(response => {
-                    this.items = response.data
-                })
-                .catch(error => console.log(error))
+            getData() {
+                axios.get(url)
+                    .then(response => this.items = response.data)
+                    .catch(error => console.log(error))
             }
         },
-    created(){
-        this.getData()
-        !this.$store.state.auth.user ? this.$router.push({name: 'login'}) : '';
-        this.page = this.$router.app._route.query.page ? this.$router.app._route.query.page : 1;
-    }
+        created() {
+            this.getData();
+            if (!this.$store.state.auth.user) this.$router.push({
+                name: 'login'
+            });
+            // this.page = this.$router.app._route.query.page ? this.$router.app._route.query.page : 1;
+        }
 
-}
+    }
 </script>
